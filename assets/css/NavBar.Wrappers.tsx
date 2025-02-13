d8e8a007b752729969c0cf9da38d961b728b1864
madhan rajesh <mrmadhan1327@gmail.com>
2025-02-13 18:00:51 +0530

    fix navbar color

--- assets/css/NavBar.Wrappers.tsx
+++ assets/css/NavBar.Wrappers.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Wrappers = styled.div`
   /* background-color: red; */
   margin-bottom: -8rem;
-  background-color: var(--primary-900);
+  background-color: #000c24;
   position: sticky;
   z-index: 1000;
   top: -1px;
